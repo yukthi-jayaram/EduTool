e37834a28b4c1fe8a58bad5cdaec28b41963819b
Jayaram Raju V <engg.jayaram@gmail.com>
2015-02-03 20:17:58 +0530

    commit

--- src/main/java/org/yukthi/edutool/service/AdmissionService.java
+++ src/main/java/org/yukthi/edutool/service/AdmissionService.java
@@ -3,7 +3,10 @@
  */
 package org.yukthi.edutool.service;
 
+import java.util.UUID;
+
 import org.yukthi.edutool.entity.Admission;
+import org.yukthi.edutool.entity.Payment;
 import org.yukthi.edutool.exception.ServiceException;
 
 /**
@@ -14,5 +17,10 @@ public interface AdmissionService {
 
 	void admitStudent(Admission admission )throws ServiceException;
 	
-	void de
+	void payAdmissionFee(UUID admissionID , Payment payment)throws ServiceException;
+	
+	void deabmitStudent(Admission admission)throws ServiceException;
+	
+	void payAdmissionFeeDeu(UUID admissionID , Payment payment)throws ServiceException;
+	
 }
